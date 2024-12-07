4abf1228c615584f23b0e1acde0d438812ff6231
Jiaqi Huang <jiaqihuang@smail.nju.edu.cn>
2024-12-07 19:22:42 +0800

    finished lab6

--- src/router.cc
+++ src/router.cc
@@ -21,10 +21,59 @@ void Router::add_route( const uint32_t route_prefix,
        << " on interface " << interface_num << "\n";
 
   // Your code here.
+  route_table_.push_back( { route_prefix, prefix_length, next_hop, interface_num } );
 }
 
 // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
 void Router::route()
 {
   // Your code here.
+  for ( auto& interface : _interfaces ) {
+    while ( !interface->datagrams_received().empty() ) {
+      int8_t max_prefix_length = -1;
+      auto interface_num = -1;
+      optional<Address> next_hop;
+      auto dgram = interface->datagrams_received().front();
+      interface->datagrams_received().pop();
+      auto dst = dgram.header.dst;
+      if ( dgram.header.ttl == 0 ) {
+        std::cerr << "TTL is 0, dropping packet\n";
+        continue;
+      }
+      dgram.header.ttl--;
+      if ( dgram.header.ttl == 0 ) {
+        std::cerr << "TTL is 0, dropping packet\n";
+        continue;
+      }
+      dgram.header.compute_checksum();
+      for ( auto& route : route_table_ ) {
+        if ( route.prefix_length < max_prefix_length )
+          continue;
+        int i = -1;
+        for ( i = 0; i < route.prefix_length; i++ ) {
+          if ( ( dst & ( 1 << ( 31 - i ) ) ) != ( route.route_prefix & ( 1 << ( 31 - i ) ) ) )
+            break;
+          // if ( i == route.prefix_length - 1 && route.prefix_length >= max_prefix_length ) {
+          //   max_prefix_length = route.prefix_length;
+          //   next_hop = route.next_hop;
+          //   interface_num = route.interface_num;
+          // }
+        }
+        if ( i == route.prefix_length && route.prefix_length >= max_prefix_length ) {
+          max_prefix_length = route.prefix_length;
+          next_hop = route.next_hop;
+          interface_num = route.interface_num;
+        }
+      }
+      if ( max_prefix_length == -1 ) {
+        std::cerr << "No route found, dropping packet\n";
+        continue;
+      }
+      if ( next_hop.has_value() ) {
+        _interfaces[interface_num]->send_datagram( dgram, next_hop.value() );
+      } else {
+        _interfaces[interface_num]->send_datagram( dgram, Address::from_ipv4_numeric( dgram.header.dst ) );
+      }
+    }
+  }
 }
